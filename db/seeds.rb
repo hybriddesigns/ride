e838e800b6d16bfd96fa74b7582250924a693033
ameerh <ameer@novatoresols.com>
2014-11-18 16:21:07 +0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Driver.create(name: "Abdullah Abdullah", location_lat: "31.480136", location_long: "74.280311")
+Driver.create(name: "Abdullah Abdullah", location_lat: "31.480136", location_long: "74.280311")
 
 
-# Driver.create(name: "Ahmed", location_lat: "31.478736", location_long: "74.282768")
+Driver.create(name: "Ahmed", location_lat: "31.478736", location_long: "74.282768")
 
 
-# Driver.create(name: "Munir", location_lat: "31.478334", location_long: "74.275655")
+Driver.create(name: "Munir", location_lat: "31.478334", location_long: "74.275655")
 
 
-# Driver.create(name: "Mughal Akbar", location_lat: "31.479953", location_long: "74.283401")
+Driver.create(name: "Mughal Akbar", location_lat: "31.479953", location_long: "74.283401")
 
 
-# Driver.create(name: "Saad Jatt", location_lat: "31.477549", location_long: "74.281636")
+Driver.create(name: "Saad Jatt", location_lat: "31.477549", location_long: "74.281636")
 
 
-# Driver.create(name: "Ali Zahid Khan", location_lat: "31.471359", location_long: "74.277827")
+Driver.create(name: "Ali Zahid Khan", location_lat: "31.471359", location_long: "74.277827")
 
 
-# Driver.create(name: "Fraz Jalandhari", location_lat: "31.469222", location_long: "74.298641")
+Driver.create(name: "Fraz Jalandhari", location_lat: "31.469222", location_long: "74.298641")
 
 
-# Driver.create(name: "Ahmed Sharq", location_lat: "31.459046", location_long: "74.302782")
+Driver.create(name: "Ahmed Sharq", location_lat: "31.459046", location_long: "74.302782")
 
 
-# Driver.create(name: "Khan Abdul Qayyum Khan", location_lat: "31.483295", location_long: "74.298126")
-#  
+Driver.create(name: "Khan Abdul Qayyum Khan", location_lat: "31.483295", location_long: "74.298126")
