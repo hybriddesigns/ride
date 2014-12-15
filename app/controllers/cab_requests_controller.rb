fc8523935d6125b485a8970f6ecea667d5678781
ameer <ameer@novatoresols.com>
2014-12-15 15:55:59 +0500

    fixed drivers for testing

--- app/controllers/cab_requests_controller.rb
+++ app/controllers/cab_requests_controller.rb
@@ -112,7 +112,7 @@ class CabRequestsController < ApplicationController
             if (!@cab_request.options_flag) # first time rejection
               send_more_locations_to_customer(@cab_request, @short_code) #send more options
             else # on rejection twice. delete the request and show "ask others" message
-              @message = "No more locations. Please ask near by people the correct spelling to your location and send message again"
+              @message = "Please ask near by people the correct spelling to your location and send message again, Or try different name to the location"
               send_message(@cell_no, @message, @short_code)
               @cab_request.delete 
             end       
@@ -182,9 +182,7 @@ class CabRequestsController < ApplicationController
         @location_to_confirm = @result["results"][0]["address_components"][0]['long_name']
         @cab_request = CabRequest.new
         @cab_request.register_request(customer_cell_no, lat, long, location)
-        @message   = "Please SMS back Y If this is your location \n"
-        @message  += " - " + @location_to_confirm + "\n"
-        @message  += "Please SMS back N If this is not your location \n"
+        @message   = '"Is your pick-up location "('+@location_to_confirm+')?" SMS Y for Yes, N for No'
         send_message(customer_cell_no, @message, short_code)        
       else # If location is invalid and no result from Google API
         @message = "Please ask near by people the correct spelling to your location and send message again"
