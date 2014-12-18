0b52bb0b99853114b76b79b85101e08d7d5b1db8
frazn-sol <fraz@novatoresols.com>
2014-12-18 20:46:06 +0500

    user authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :locations
 
-  root 'cab_requests#new'
+  root 'location#index'
   get  'cab_requests/receive_sms' => 'cab_requests#receive_sms'
   # get  'cab_requests/receive_sms_for_ride' => 'cab_requests#receive_sms_for_ride'
   # get  'cab_requests/receive_sms_for_driver_registration' => 'cab_requests#receive_sms_for_driver_registration'
